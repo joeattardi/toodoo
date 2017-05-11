b191ab1fcc8d43ada55cf0e305aac44a19423d8c
Joe Attardi <jattardi@constantcontact.com>
2017-05-11 13:35:11 -0400

    test works

--- src/app/todos/todo-list/todo-item/todo-item.component.spec.ts
+++ src/app/todos/todo-list/todo-item/todo-item.component.spec.ts
@@ -1,14 +1,24 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+import { Todo } from '../../todo.model';
+import { DragDropService } from '../../../drag-drop.service';
 import { TodoItemComponent } from './todo-item.component';
 
-describe('TodoItemComponent', () => {
+fdescribe('TodoItemComponent', () => {
   let component: TodoItemComponent;
   let fixture: ComponentFixture<TodoItemComponent>;
 
+  let todoText: HTMLElement;
+  let dueDate: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoItemComponent ]
+      declarations: [ TodoItemComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [ DragDropService, { provide: NgbModal, useValue: {} } ]
     })
     .compileComponents();
   }));
@@ -16,10 +26,20 @@ describe('TodoItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoItemComponent);
     component = fixture.componentInstance;
+    const todo = new Todo('1', 'Walk the dog', false, new Date(2017, 5, 12));
+    component.todo = todo;
     fixture.detectChanges();
+
+    todoText = fixture.debugElement.query(By.css('.todo-text div')).nativeElement;
+    dueDate = fixture.debugElement.query(By.css('.due-date')).nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the todo details', () => {
+    expect(todoText.innerHTML).toBe('Walk the dog');
+    expect(dueDate.innerHTML).toBe('Jun 12, 2017');
   });
 });
